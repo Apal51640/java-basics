3529745b2d592cd01690ec14b13cc4c10666bb34
Abhishek Pal <apal51640@gmail.com>
2023-06-02 13:43:10 +0530

    Arithmetic operators

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        int a = 10, b = 15;
+        int c = a+b;
+        int d = a-b;
+        int e = a*b;
+        int f = a/b;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
